06cae811e5f4270c07c29ad38c1559e590b35199
Dzaki Akmal Rabbani Alqadrie <dzakirabbania@gmail.com>
2025-03-23 17:06:17 +0700

    Fix Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import { HiMiniBars3CenterLeft } from "react-icons/hi2";
 import { IoSearchOutline } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi";
+import { HiOutlineHeart } from "react-icons/hi";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 
 const Navbar = () => {
     return (
@@ -9,24 +12,31 @@ const Navbar = () => {
                 {/*left side */}
                 <div className="flex items-center md:gap-16 gap-4">
                     <Link to="/">
-                    <HiMiniBars3CenterLeft className="size-6"/>
+                        <HiMiniBars3CenterLeft className="size-6" />
                     </Link>
 
                     {/* search input */}
                     <div className="relative sm:w-72 w-40 space-x-2">
 
-                        <IoSearchOutline className="absolute inline-block left-3 inset-y-2"/>
+                        <IoSearchOutline className="absolute inline-block left-3 inset-y-2" />
 
-                        <input type="text" placeholder="Search here" 
-                        className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md 
+                        <input type="text" placeholder="Search here"
+                            className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md 
                         focus:ouline-none"
                         />
                     </div>
                 </div>
 
                 {/*Right side */}
-                <div>
-                    nav items
+                <div className="relative flex items-center md:space-x-3 space-x-2">
+                    <HiOutlineUser className="size-6" />
+                    <button className="sm:block">
+                        <HiOutlineHeart className="size-6" />
+                    </button>
+                    <Link to='/cart' className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-sm">
+                        <HiOutlineShoppingCart className="" />
+                        <span className="text-sm font-semibold sm:ml-1">0</span>
+                    </Link>
                 </div>
             </nav>
         </header>
