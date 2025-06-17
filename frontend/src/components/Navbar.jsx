98aab651fa10d4f5fbb6617f531283e41a5944a6
Dzaki Akmal Rabbani Alqadrie <dzakirabbania@gmail.com>
2025-06-17 22:34:24 +0700

    hapus search bar & 3 baris

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -34,18 +34,10 @@ const Navbar = () => {
                 {/* left side */}
                 <div className="flex items-center md:gap-16 gap-4">
                     <Link to="/">
-                        <HiMiniBars3CenterLeft className="size-6" />
+                        
                     </Link>
 
-                    {/* search input */}
-                    <div className="relative sm:w-72 w-40 space-x-2">
-
-                        <IoSearchOutline className="absolute inline-block left-3 inset-y-2" />
-
-                        <input type="text" placeholder="Search here"
-                            className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
-                        />
-                    </div>
+                    
                 </div>
 
 
