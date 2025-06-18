09467713ff0c3e4bb4f428c5d2c44e43d1c907b2
Dzaki Akmal Rabbani Alqadrie <dzakirabbania@gmail.com>
2025-06-18 17:29:00 +0700

    Ganti link cors di injex.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://bookstore-mern-ppl.vercel.app/'],
+    origin: ['https://bookstore-mern-ppl.vercel.app/'],
     credentials: true
 }))
 
